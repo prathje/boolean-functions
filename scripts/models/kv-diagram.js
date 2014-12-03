f097df7f65ede1dc7ce285349dbbfe7765695dfa
Slin91995 <nils.ziermann@freenet.de>
2014-12-03 21:25:52 +0100

    Added Wertetabelle

--- scripts/models/kv-diagram.js
+++ scripts/models/kv-diagram.js
@@ -1,10 +1,18 @@
 var Wertetabelle = Backbone.Model.extend({
 	defaults: {
 		numVariables: 0,
+		namesVariables: [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
 		minTerms: []
 	}
 });
 
+var Wertetabellereihe = Backbone.Model.extend({
+	defaults: {
+		numVariables: 0,
+		minTerm: ""
+	}
+});
+	
 var kvTabelle = Wertetabelle.extend({
 	defaults: {
 		rows: 0,
--- scripts/views/page.js
+++ scripts/views/page.js
@@ -1,17 +1,17 @@
 var kvTabelleView = Backbone.View.extend({
 	el: $('#container-kv'),
 	initialize: function(){
-		this.render;
+		this.render();
 		this.model.on("change", this.render);
 	},
 	render: function(){
 		var s = "<table>";
-		for(var a = 0; a = this.model.get("columns"); ++a)
+		for(var a = 0; a < this.model.get("columns"); ++a)
 		{
 			s = "<tr>";
-			for(var b = 0; b = this.model.get("rows"); ++b)
+			for(var b = 0; b < this.model.get("rows"); ++b)
 			{
-				s+= "<th id='" + (a*this.model.get("rows") + b) + "'>" + this.model.get("KVCells[" + (a*this.model.rows + b)+ "]") + "</th>";
+				s+= "<th id='" + (a*this.model.get("rows") + b) + "'>" + this.model.get("KVCells")[(a*this.model.get("rows") + b)] + "</th>";
 			}
 			s += "</tr>";
 			this.$el.append(s);
@@ -19,5 +19,36 @@ var kvTabelleView = Backbone.View.extend({
 		s = "</table>";
 		this.$el.append(s);
 	}
+});
+
+var WertetabellereiheView = Backbone.View.extend({
+	initialize: function(){
+		this.render();
+	},
+	render: function(){
+		var s = "<tr>";
+		for(var i = 0; i < this.model.get("numVariables"); ++i){
+			s += "<th id='" + i + "'>" + this.model.get("namesVariables")[this.model.get("numVariables")-i] + "</th>"; 
+		}
+	}
 })
+
+var WertetabelleView = Backbone.View.extend({
+	initialize: function(){
+		this.model.on("change", this.render);
+	},
+	render: function(){
+		this.$el.empty();
+		this.$el.append("<table>");
+		for(var a = 0; a < Math.pow(2, this.model.get("numVariables")); ++i){
+			var s = "<tr>";
+			for(var i = 0; i < this.model.get("numVariables"); ++i){
+				s += "<th id='" + i + "'>" + this.model.get("namesVariables")[this.model.get("numVariables")-i] + "</th>"; 
+			}
+			s += "</tr>";
+			this.$el.append.(s);
+		}
+		this.$el.append("<table>");
+	}
+}
 	
